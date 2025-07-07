3a061ce3272fde7da8687e0241590bcf6329c89c
Sabbir-Noyon <sabbir.noyon.001@gmail.com>
2025-07-08 02:18:24 +0600

    fix key issue

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,16 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// Your web app's Firebase configuration from environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyC4SYjNZActDTShwaiNOnjDK4xdJHZQuIA",
-  authDomain: "project-corporate-solution.firebaseapp.com",
-  projectId: "project-corporate-solution",
-  storageBucket: "project-corporate-solution.appspot.com",
-  messagingSenderId: "347475682769",
-  appId: "1:347475682769:web:d9ec24c5a8825ca1dcacf0"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
